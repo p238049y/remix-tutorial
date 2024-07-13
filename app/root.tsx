bf63c4bca823fa41bd816f4109f13de6e5dd8b09
p238049y <p238049y@gmail.com>
2024-07-13 23:22:17 +0900

    [update] サイドメニューの検索機能を追加
    [タスク] チュートリアルをやってみる #1

--- app/root.tsx
+++ app/root.tsx
@@ -24,8 +24,12 @@ export const links: LinksFunction = () => [
 ];
 
 // NOTE: exportしないとUI上に表示されない
-export const loader = async () => {
-  const contacts = await getContacts();
+export const loader = async ({
+  request,
+}: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
   return json({ contacts });
 };
 
