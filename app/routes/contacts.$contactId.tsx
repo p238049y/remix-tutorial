6b0084f27b0a0bd6cb7cf1b6a4a15964e2e39d0c
p238049y <p238049y@gmail.com>
2024-07-13 23:56:30 +0900

    [update] 選択した連絡先の詳細を表示するためにloaderの処理を修正
    [タスク] チュートリアルをやってみる #1

--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -1,20 +1,20 @@
-import { Form, json, useFetcher } from "@remix-run/react";
+import { Form, json, useFetcher, useLoaderData } from "@remix-run/react";
 import type { FunctionComponent } from "react";
 
-import { getContacts, updateContact, type ContactRecord } from "../data";
+import { getContact, getContacts, updateContact, type ContactRecord } from "../data";
 import { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
 import invariant from "tiny-invariant";
 
 export const loader = async ({params}: LoaderFunctionArgs) => {
   invariant(params.contactId, "Missing contactId param");
 
-  const contacts = await getContacts(params.contactId);
+  const contact = await getContact(params.contactId);
 
-  if (!contacts) {
+  if (!contact) {
     throw new Response("Not Found", { status: 404 });
   }
 
-  return json({ contacts });
+  return json({ contact });
 };
 
 export const action = async ({
@@ -29,71 +29,64 @@ export const action = async ({
 };
 
 const Contact = () => {
-  const contact = {
-      first: "Your",
-      last: "Name",
-      avatar: "https://sessionize.com/image/124e-400o400o2-wHVdAuNaxi8KJrgtN3ZKci.jpg",
-      twitter: "your_handle",
-      notes: "Some notes",
-      favorite: true,
-    };
+  const { contact } = useLoaderData<typeof loader>();
   
-    return (
-      <div id="contact">
-        <div>
-          <img
-            alt={`${contact.first} ${contact.last} avatar`}
-            key={contact.avatar}
-            src={contact.avatar}
-          />
-        </div>
-  
-        <div>
-          <h1>
-            {contact.first || contact.last ? (
-              <>
-                {contact.first} {contact.last}
-              </>
-            ) : (
-              <i>No Name</i>
-            )}{" "}
-            <Favorite contact={contact} />
-          </h1>
-  
-          {contact.twitter ? (
-            <p>
-              <a
-                href={`https://twitter.com/${contact.twitter}`}
-              >
-                {contact.twitter}
-              </a>
-            </p>
-          ) : null}
-  
-          {contact.notes ? <p>{contact.notes}</p> : null}
-  
-          <div>
-            <Form action="edit">
-              <button type="submit">Edit</button>
-            </Form>
-  
-            <Form
-              action="destroy"
-              method="post"
-              onSubmit={(event) => {
-                const response = confirm(
-                  "Please confirm you want to delete this record."
-                );
-                if (!response) {
-                  event.preventDefault();
-                }
-              }}
+  return (
+    <div id="contact">
+      <div>
+        <img
+          alt={`${contact.first} ${contact.last} avatar`}
+          key={contact.avatar}
+          src={contact.avatar}
+        />
+      </div>
+
+      <div>
+        <h1>
+          {contact.first || contact.last ? (
+            <>
+              {contact.first} {contact.last}
+            </>
+          ) : (
+            <i>No Name</i>
+          )}{" "}
+          <Favorite contact={contact} />
+        </h1>
+
+        {contact.twitter ? (
+          <p>
+            <a
+              href={`https://twitter.com/${contact.twitter}`}
             >
-              <button type="submit">Delete</button>
-            </Form>
-          </div>
+              {contact.twitter}
+            </a>
+          </p>
+        ) : null}
+
+        {contact.notes ? <p>{contact.notes}</p> : null}
+
+        <div>
+          <Form action="edit">
+            <button type="submit">Edit</button>
+          </Form>
+
+          <Form
+            action="destroy"
+            method="post"
+            onSubmit={(event) => {
+              const response = confirm(
+                "Please confirm you want to delete this record."
+              );
+              if (!response) {
+                event.preventDefault();
+              }
+            }}
+          >
+            <button type="submit">Delete</button>
+          </Form>
         </div>
       </div>
+    </div>
   );
 }
 
